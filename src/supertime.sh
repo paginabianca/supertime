1611359e3fce31ae2872f2ee57d69dde880f195a
Andreas Botzner <paginabianca66@gmail.com>
2022-03-22 09:56:59 +0100

    Some fixes in the BASH code thanks to `shellcheck`
    Multiple fixes with wrong file/dir checks and empty strings.

--- src/supertime.sh
+++ src/supertime.sh
@@ -6,7 +6,7 @@
 
 set -eou pipefail
 
-FILE=~/.local/share/timings
+FILE=~/.local/share/supertime/supertime.csv
 TIME_ARGS='-f %U,%S,%P,%E,%C,%x --'
 HASH=""
 
@@ -14,40 +14,35 @@ if [ ! -d ~/.cache/supertime ]; then
     mkdir -p ~/.cache/supertime
 fi
 
-if [ ! -s ~/.local/share/supertime ]; then
-    mkdir -p ~/.local/share/supertime
+if [ ! -d "$(dirname ${FILE})" ]; then
+    mkdir -p "$(dirname ${FILE})"
 fi
 
-if [ ! -e ~/.local/share/supertime/timings.csv ]; then
-    echo "User Time,System Time,CPU,Total Time,Command/File,Commit;" >> ~/.local/share/supertime/timings.csv
+if [ ! -e ${FILE} ]; then
+    touch ${FILE}
+    echo "User Time,System Time,CPU,Total Time,Command/File,Exit Code,Commit;" > $FILE
 fi
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
     echo "First argument has to be file to be executed."
-    exit -1
+    exit 1
 fi
 
-if $(git status 2>/dev/null 1>/dev/null); then
+if git status 2>/dev/null 1>/dev/null; then
     # CWD is GIT VCS
     if  [ -n "$(git status --porcelain 2>&1)" ]; then
       # Uncommitted changes
       echo "Git dir should be clean. Commit all you changes and try again."
-      exit -1
+      exit 1
     else
-        HASH=$(git rev-parse --short)
+        HASH=$(git rev-parse --short HEAD)
     fi
 fi
 
 UUID=~/.cache/supertime/$(uuidgen)
 touch $UUID
-TIME=$(/usr/bin/time --output-file=${UUID} ${TIME_ARGS} $1)
+/usr/bin/time --output-file="${UUID}" "${TIME_ARGS}" "$1"
 
-if [ ! -z $HASH ]; then
-    echo "hash is nonzero '$HASH'"
-    MEASUREMENT="$(/usr/bin/cat ${UUID}),$HASH;"
-else
-    echo "hash zero '$HASH'"
-    MEASUREMENT="$(/usr/bin/cat ${UUID});"
-fi
+MEASUREMENT="$(/usr/bin/cat "${UUID}"),$HASH;"
 
 echo "$MEASUREMENT" >> ${FILE}
